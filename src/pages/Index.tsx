402adf589c30f1fa314dedb309b62404736d624c
sukritvemula <sukritvemula@outlook.com>
2025-02-21 22:49:35 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -116,19 +116,19 @@ const Index = () => {
     {
       name: 'GitHub',
       icon: Github,
-      href: '#',
+      href: 'https://github.com/cbitosc',
       hoverClass: 'hover:bg-[#333]/20 hover:text-white hover:shadow-white/20'
     },
     {
       name: 'Instagram',
       icon: Instagram,
-      href: '#',
+      href: 'https://www.instagram.com/cbitosc/',
       hoverClass: 'hover:bg-[#333]/20 hover:text-white hover:shadow-white/20'
     },
     {
       name: 'LinkedIn',
       icon: Linkedin,
-      href: '#',
+      href: 'https://www.linkedin.com/company/cbitosc',
       hoverClass: 'hover:bg-[#333]/20 hover:text-white hover:shadow-white/20'
     }
   ];
