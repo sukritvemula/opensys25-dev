2b5fdd354abc4aa95f8c0b5e7823de4df0362e20
sukritvemula <sukritvemula@outlook.com>
2025-02-22 13:11:36 +0530

    Add files via upload

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+const Logo = () => {
+  return (
+    <div className="flex flex-col items-center">
+      {/* "CO" on top */}
+      <span className="font-sora font-bold text-2xl text-white leading-none">CO</span>
+      {/* "SC" below */}
+      <span className="font-sora font-bold text-2xl text-white leading-none">SC</span>
+    </div>
+  );
+};
+
+export default Logo;
